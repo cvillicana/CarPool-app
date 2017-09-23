9bb0eba7839197b9f8eef92135c090749db5ae38
Unknown <villicana.ca@gmail.com>
2017-09-23 10:50:26 -0500

    set dev

--- src/app/environment-variables/development.ts
+++ src/app/environment-variables/development.ts
@@ -1,5 +1,5 @@
 export const devVariables = {
-  apiEndpoint: 'http://192.168.1.76:8080/',
+  apiEndpoint: 'https://carpool-server.herokuapp.com/',
   environmentName: 'Development Environment',
   fbAppId: '1290362954408363',
   ionicEnvName: 'dev'
